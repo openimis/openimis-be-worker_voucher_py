a22122d079e6d221e04b9421dbc03f1ec331ddf7
Kamil Malinowski <kmalinowski@soldevelo.com>
2023-11-24 09:30:01 +0100

    OM-77 Implemented voucher ORM

--- worker_voucher/models.py
+++ worker_voucher/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from core.models import HistoryModel
+from core import fields
+from insuree.models import Insuree
+from policyholder.models import PolicyHolder
+
+
+class WorkerVoucher(HistoryModel):
+    class Status(models.TextChoices):
+        PENDING = 'AWAITING_PAYMENT', _('Awaiting Payment')
+        UNASSIGNED = 'UNASSIGNED', _('Unassigned')
+        ASSIGNED = 'ASSIGNED', _('Assigned')
+        EXPIRED = 'EXPIRED', _('Expired')
+        CANCELED = 'CANCELED', _('Canceled')
+        CLOSED = 'CLOSED', _('Closed')
+
+    insuree = models.ForeignKey(Insuree, null=True, blank=True, on_delete=models.DO_NOTHING)
+    policyholder = models.ForeignKey(PolicyHolder, null=True, blank=True, on_delete=models.DO_NOTHING)
+    code = models.CharField(max_length=255, blank=True, null=True)
+    status = models.CharField(max_length=255, blank=True, null=True, choices=Status.choices, default=Status.PENDING)
+    assigned_date = fields.DateField(blank=True, null=True)
+    expiry_date = fields.DateField(blank=True, null=True)
