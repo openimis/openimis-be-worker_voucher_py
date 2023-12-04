0f7cf01d4759d742b8f00af160a6bb220e6aaa62
Kamil Malinowski <kmalinowski@soldevelo.com>
2023-12-04 14:31:35 +0100

    OM-91 Added previousWorkers query

--- worker_voucher/schema.py
+++ worker_voucher/schema.py
@@ -2,9 +2,13 @@ import graphene
 import graphene_django_optimizer as gql_optimizer
 
 from django.db.models import Q
+from django.utils.translation import gettext as _
 from django.contrib.auth.models import AnonymousUser
 from core.schema import OrderedDjangoFilterConnectionField
 from core.utils import append_validity_filter
+from insuree.apps import InsureeConfig
+from insuree.gql_queries import InsureeGQLType
+from insuree.models import Insuree
 from worker_voucher.apps import WorkerVoucherConfig
 from worker_voucher.gql_queries import WorkerVoucherGQLType
 from worker_voucher.models import WorkerVoucher
@@ -19,6 +23,12 @@ class Query(graphene.ObjectType):
         client_mutation_id=graphene.String(),
     )
 
+    previous_workers = OrderedDjangoFilterConnectionField(
+        InsureeGQLType,
+        orderBy=graphene.List(of_type=graphene.String),
+        economic_unit_code=graphene.String(required=True)
+    )
+
     def resolve_worker_voucher(self, info, **kwargs):
         Query._check_permissions(info.context.user, WorkerVoucherConfig.gql_worker_voucher_search_perms)
         filters = append_validity_filter(**kwargs)
@@ -30,7 +40,19 @@ class Query(graphene.ObjectType):
         query = WorkerVoucher.objects.filter(*filters)
         return gql_optimizer.query(query, info)
 
+    def resolve_previous_workers(self, info, economic_unit_code=None, **kwargs):
+        Query._check_permissions(info.context.user, InsureeConfig.gql_query_insuree_perms)
+        filters = append_validity_filter(**kwargs)
+
+        query = Insuree.get_queryset(None, info.context.user).filter(
+            workervoucher__is_deleted=False,
+            workervoucher__policyholder__is_deleted=False,
+            workervoucher__policyholder__code=economic_unit_code
+        )
+
+        return gql_optimizer.query(query, info)
+
     @staticmethod
     def _check_permissions(user, perms):
         if type(user) is AnonymousUser or not user.id or not user.has_perms(perms):
-            raise PermissionError("Unauthorized")
+            raise PermissionError(_("Unauthorized"))
