ae84d7b032fc48a53b0ac600214a7baf5eba5597
sniedzielski <sniedzielski@soldevelo.com>
2024-10-30 12:10:06 +0100

    OM-324: added missing helper methods to tests

--- worker_voucher/tests/test_voucher_form_draft_create.py
+++ worker_voucher/tests/test_voucher_form_draft_create.py
@@ -126,3 +126,13 @@ class GQLVoucherDraftFormCreateTestCase(TestCase):
             type="ASSIGNMENT"
         )
         self.assertEquals(draft.count(), 0)
+
+    def _assert_mutation_success(self, mutation_id):
+        mutation_log = MutationLog.objects.get(client_mutation_id=mutation_id)
+        self.assertEquals(mutation_log.status, 2)
+        self.assertFalse(mutation_log.error)
+
+    def _assert_mutation_failed(self, mutation_id):
+        mutation_log = MutationLog.objects.get(client_mutation_id=mutation_id)
+        self.assertEquals(mutation_log.status, 1)
+        self.assertTrue(mutation_log.error)
--- worker_voucher/tests/test_voucher_form_draft_update.py
+++ worker_voucher/tests/test_voucher_form_draft_update.py
@@ -126,3 +126,13 @@ class GQLVoucherDraftFormUpdateTestCase(TestCase):
         self.assertEquals(draft.type, "ASSIGNMENT")
         self.assertEquals(draft_dates.count(), 2)
         self.assertEquals(draft_workers.count(), 3)
+
+    def _assert_mutation_success(self, mutation_id):
+        mutation_log = MutationLog.objects.get(client_mutation_id=mutation_id)
+        self.assertEquals(mutation_log.status, 2)
+        self.assertFalse(mutation_log.error)
+
+    def _assert_mutation_failed(self, mutation_id):
+        mutation_log = MutationLog.objects.get(client_mutation_id=mutation_id)
+        self.assertEquals(mutation_log.status, 1)
+        self.assertTrue(mutation_log.error)
